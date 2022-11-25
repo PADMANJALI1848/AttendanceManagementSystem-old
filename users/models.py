19ce6ffae04bb2a65a6cf9519e6d029a433fe4be
Veda <vedacr2510@gmail.com>
2022-11-25 18:04:35 +0530

    created models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.TextField(max_length=120)
+    name= models.TextField(blank=True , null=True)
+    email = models.TextField(max_length=120)
+    password= models.TextField(max_length=120)
+    confirmpassword= models.TextField(max_length=120)
